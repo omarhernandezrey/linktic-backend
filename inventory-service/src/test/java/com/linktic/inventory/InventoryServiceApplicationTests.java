7a6372e90f7c32670f706caac5d91bd7420e9539
omarhernandezrey <hernandezreyomar@gmail.com>
2025-10-27 22:04:15 -0500

    chore(build): fix maven compiler config, set java.version=21; add .gitignore

--- inventory-service/src/test/java/com/linktic/inventory/InventoryServiceApplicationTests.java
+++ inventory-service/src/test/java/com/linktic/inventory/InventoryServiceApplicationTests.java
@@ -6,7 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class InventoryServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
+
 }
---
+++ products-service/src/test/java/com/linktic/productsservice/ProductsServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.linktic.productsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
